0cb035b1c26365625f6412ae5a5bdecf47a86f2e
quevedoana <et.quevedoana@gmail.com>
2026-06-11 21:01:54 -0300

    fix: arreglo para seed

--- db/config.js
+++ db/config.js
@@ -1,14 +1,15 @@
+import "dotenv/config";
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host:process.env.DB_HOST,
-    username:process.env.DB_USER,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 
-      dialectOptions:
+  dialectOptions:
     process.env.NODE_ENV === "production"
       ? {
           ssl: {
@@ -17,8 +18,6 @@ const sequelize = new Sequelize({
           },
         }
       : {},
-})
+});
 
-
-
-export default sequelize
+export default sequelize;
