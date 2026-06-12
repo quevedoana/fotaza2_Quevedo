2489b5245b6c61aa57994c213c6eac5607ae2525
quevedoana <et.quevedoana@gmail.com>
2026-06-11 21:10:10 -0300

    Revert "fix: arreglo para seed"
    This reverts commit 0cb035b1c26365625f6412ae5a5bdecf47a86f2e.

--- db/config.js
+++ db/config.js
@@ -1,15 +1,14 @@
-import "dotenv/config";
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+    dialect: 'postgres',
+    host:process.env.DB_HOST,
+    username:process.env.DB_USER,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 
-  dialectOptions:
+      dialectOptions:
     process.env.NODE_ENV === "production"
       ? {
           ssl: {
@@ -18,6 +17,8 @@ const sequelize = new Sequelize({
           },
         }
       : {},
-});
+})
 
-export default sequelize;
+
+
+export default sequelize
